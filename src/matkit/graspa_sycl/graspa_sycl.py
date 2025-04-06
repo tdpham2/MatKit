207458da27b0cfad56a4201d8226109a3b89aa45
Thang Pham <tpham@anl.gov>
2025-04-06 08:53:15 -0500

    Add g/L unit for gRASPA-sycl data output

--- src/matkit/graspa_sycl/graspa_sycl.py
+++ src/matkit/graspa_sycl/graspa_sycl.py
@@ -73,17 +73,19 @@ def get_output_data(
     unit: str = "mol/kg",
     output_fname: str = "raspa.log",
     cifname: str = None,
+    adsorbate: str = "CO2",
 ):
     result = {"success": False, "uptake": 0, "error": 0, "unit": unit}
     try:
-        with open(os.path.join(output_path, output_fname), "r") as file:
-            for line in file:
+        with open(os.path.join(output_path, output_fname), "r") as rf:
+            for line in rf:
                 if "UnitCells" in line:
                     unitcell_line = line.strip()
                 elif "Overall: Average:" in line:
                     uptake_line = line.strip()
                 elif "Work" in line:
                     time_line = line.strip()
+        result["calc_time_in_s"] = float(time_line.split()[2])
 
         if cifname is None:
             cif_list = glob.glob(os.path.join(output_path, "*.cif"))
@@ -94,20 +96,50 @@ def get_output_data(
         else:
             cifpath = os.path.join(output_path, cifname)
 
-        atoms = ase_read(cifpath)
-        masses = sum(atoms.get_masses())
-
         uptake_total_molecule = float(uptake_line.split()[2][:-1])
         error_total_molecule = float(uptake_line.split()[4][:-1])
         unitcell = unitcell_line.split()[4:]
         unitcell = [int(float(i)) for i in unitcell]
-        total_masses = masses * unitcell[0] * unitcell[1] * unitcell[2]
-        uptake_mol_kg = uptake_total_molecule / total_masses * 1000
-        error_mol_kg = error_total_molecule / total_masses * 1000
-        result["uptake"] = uptake_mol_kg
-        result["error"] = error_mol_kg
-        result["calc_time_in_s"] = float(time_line.split()[2])
-        result["success"] = True
+        atoms = ase_read(cifpath)
+
+        if unit == "mol/kg":
+            framework_mass = (
+                sum(atoms.get_masses())
+                * unitcell[0]
+                * unitcell[1]
+                * unitcell[2]
+            )
+
+            uptake_mol_kg = uptake_total_molecule / framework_mass * 1000
+            error_mol_kg = error_total_molecule / framework_mass * 1000
+            result["uptake"] = uptake_mol_kg
+            result["error"] = error_mol_kg
+            result["success"] = True
+        elif unit == "g/L":
+            framework_vol = (
+                atoms.get_volume * unitcell[0] * unitcell[1] * unitcell[2]
+            )
+            framework_vol_in_L = framework_vol * 1e-27
+            if adsorbate == "CO2":
+                molar_mass = 44.0098
+            elif adsorbate == "H2":
+                molar_mass = 2.02
+            else:
+                raise ValueError(f"Adsorbate {adsorbate} is not supported.")
+            uptake_g_L = (
+                uptake_total_molecule
+                / (6.022 * 1e23)
+                * molar_mass
+                / framework_vol_in_L
+            )
+            error_g_L = (
+                error_total_molecule
+                / (6.022 * 1e23)
+                * molar_mass
+                / framework_vol_in_L
+            )
+            result["uptake"] = uptake_g_L
+            result["error"] = error_g_L
 
         return result
     except Exception as e:
