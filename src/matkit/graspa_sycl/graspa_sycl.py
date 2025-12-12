d338fde9db1c4a3b024da9963a812ca8769b7333
Thang Pham <tpham@anl.gov>
2025-12-11 22:14:57 -0600

    Update setup_simulation for graspa-sycl

--- src/matkit/graspa_sycl/graspa_sycl.py
+++ src/matkit/graspa_sycl/graspa_sycl.py
@@ -8,8 +8,8 @@ import glob
 _file_dir = Path(__file__).parent / "files" / "template"
 
 
-def setup_input_simulation(
-    cifs: list[str],
+def setup_simulation(
+    cif: str,
     outpath: str,
     adsorbate: str = "CO2",
     temperature: float = 298,
@@ -19,46 +19,45 @@ def setup_input_simulation(
 ):
     outpath = Path(outpath)
 
-    for cif in cifs:
-        cifpath = Path(cif)
-        if not cifpath.exists():
-            raise FileNotFoundError(f"Source directory does not exist: {cif}")
+    cifpath = Path(cif)
+    if not cifpath.exists():
+        raise FileNotFoundError(f"Source directory does not exist: {cif}")
 
-        cifname = cif.split("/")[-1][:-4]
-        outdir = Path(os.path.join(outpath, cifname))
-        outdir.mkdir(parents=True, exist_ok=True)
-        for item in _file_dir.iterdir():
-            if item.is_dir():
-                shutil.copytree(item, outdir, dirs_exist_ok=True)
-            else:
-                shutil.copy2(item, outdir)
-        shutil.copy(cif, outdir)
-        # Editing input file.
-        atoms = ase_read(cif)
-        [uc_x, uc_y, uc_z] = calculate_cell_size(atoms)
+    cifname = cif.split("/")[-1][:-4]
+    outdir = Path(os.path.join(outpath, cifname))
+    outdir.mkdir(parents=True, exist_ok=True)
+    for item in _file_dir.iterdir():
+        if item.is_dir():
+            shutil.copytree(item, outdir, dirs_exist_ok=True)
+        else:
+            shutil.copy2(item, outdir)
+    shutil.copy(cif, outdir)
+    # Editing input file.
+    atoms = ase_read(cif)
+    [uc_x, uc_y, uc_z] = calculate_cell_size(atoms)
 
-        with (
-            open(f"{outdir}/simulation.input", "r") as f_in,
-            open(f"{outdir}/simulation.input.tmp", "w") as f_out,
-        ):
-            for line in f_in:
-                if "NCYCLE" in line:
-                    line = line.replace("NCYCLE", str(n_cycle))
-                if "ADSORBATE" in line:
-                    line = line.replace("ADSORBATE", adsorbate)
-                if "TEMPERATURE" in line:
-                    line = line.replace("TEMPERATURE", str(temperature))
-                if "PRESSURE" in line:
-                    line = line.replace("PRESSURE", str(pressure))
-                if "UC_X UC_Y UC_Z" in line:
-                    line = line.replace("UC_X UC_Y UC_Z", f"{uc_x} {uc_y} {uc_z}")
-                if "CUTOFF" in line:
-                    line = line.replace("CUTOFF", str(cutoff))
-                if "CIFFILE" in line:
-                    line = line.replace("CIFFILE", cifname)
-                f_out.write(line)
+    with (
+        open(f"{outdir}/simulation.input", "r") as f_in,
+        open(f"{outdir}/simulation.input.tmp", "w") as f_out,
+    ):
+        for line in f_in:
+            if "NCYCLE" in line:
+                line = line.replace("NCYCLE", str(n_cycle))
+            if "ADSORBATE" in line:
+                line = line.replace("ADSORBATE", adsorbate)
+            if "TEMPERATURE" in line:
+                line = line.replace("TEMPERATURE", str(temperature))
+            if "PRESSURE" in line:
+                line = line.replace("PRESSURE", str(pressure))
+            if "UC_X UC_Y UC_Z" in line:
+                line = line.replace("UC_X UC_Y UC_Z", f"{uc_x} {uc_y} {uc_z}")
+            if "CUTOFF" in line:
+                line = line.replace("CUTOFF", str(cutoff))
+            if "CIFFILE" in line:
+                line = line.replace("CIFFILE", cifname)
+            f_out.write(line)
 
-        shutil.move(f"{outdir}/simulation.input.tmp", f"{outdir}/simulation.input")
+    shutil.move(f"{outdir}/simulation.input.tmp", f"{outdir}/simulation.input")
 
     return True
 
@@ -99,7 +98,12 @@ def get_output_data(
         atoms = ase_read(cifpath)
 
         if unit == "mol/kg":
-            framework_mass = sum(atoms.get_masses()) * unitcell[0] * unitcell[1] * unitcell[2]
+            framework_mass = (
+                sum(atoms.get_masses())
+                * unitcell[0]
+                * unitcell[1]
+                * unitcell[2]
+            )
 
             uptake_mol_kg = uptake_total_molecule / framework_mass * 1000
             error_mol_kg = error_total_molecule / framework_mass * 1000
@@ -107,7 +111,9 @@ def get_output_data(
             result["error"] = error_mol_kg
             result["success"] = True
         elif unit == "g/L":
-            framework_vol = atoms.get_volume() * unitcell[0] * unitcell[1] * unitcell[2]
+            framework_vol = (
+                atoms.get_volume() * unitcell[0] * unitcell[1] * unitcell[2]
+            )
             framework_vol_in_L = framework_vol * 1e-27
             if adsorbate == "CO2":
                 molar_mass = 44.0098
@@ -115,8 +121,18 @@ def get_output_data(
                 molar_mass = 2.02
             else:
                 raise ValueError(f"Adsorbate {adsorbate} is not supported.")
-            uptake_g_L = uptake_total_molecule / (6.022 * 1e23) * molar_mass / framework_vol_in_L
-            error_g_L = error_total_molecule / (6.022 * 1e23) * molar_mass / framework_vol_in_L
+            uptake_g_L = (
+                uptake_total_molecule
+                / (6.022 * 1e23)
+                * molar_mass
+                / framework_vol_in_L
+            )
+            error_g_L = (
+                error_total_molecule
+                / (6.022 * 1e23)
+                * molar_mass
+                / framework_vol_in_L
+            )
             result["uptake"] = uptake_g_L
             result["error"] = error_g_L
 
